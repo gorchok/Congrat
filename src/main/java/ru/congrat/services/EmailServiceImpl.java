d034e761e85c878b3998910ec777bb238332dc90
Pochivalin Alexey <gorchok@mail.ru>
2018-06-21 12:04:32 +0300

    #2 build Person with PersonService and PersonForBirthday

---
+++ src/main/java/ru/congrat/services/EmailServiceImpl.java
@@ -0,0 +1,25 @@
+package ru.congrat.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailServiceImpl {
+
+    @Autowired
+    public JavaMailSender emailSender;
+
+    public void sendSimpleMessage(String from,String to, String subject, String text) {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setFrom(from);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+        System.out.println("Message send");
+    }
+
+}
--- src/main/java/ru/congrat/sheduled/ScheduledTasks.java
+++ src/main/java/ru/congrat/sheduled/ScheduledTasks.java
@@ -1,6 +1,7 @@
 package ru.congrat.sheduled;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -10,12 +11,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import ru.congrat.model.Person;
+import ru.congrat.services.EmailServiceImpl;
 
 @Component
 public class ScheduledTasks {
 
     @Autowired
     PersonForBirthday personForBirthday;
+    @Autowired
+    EmailServiceImpl emailService;
 
     //private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
 
@@ -27,5 +31,13 @@ public class ScheduledTasks {
         System.out.println("The time is now {}: " + dateFormat.format(new Date()));
         List<Person> list = personForBirthday.personFilter();
         System.out.println(list);
+
+        List<String> email = new ArrayList<String>();
+        email.add("a.pochivalin@answerpro.org");
+        email.add("gorchok@mail.ru");
+        for (String e:email) {
+        emailService.sendSimpleMessage("gorchok@mail.ru",e,"Поздравление","С Днем Рождения ! ! !");
+        }
+
     }
 }
